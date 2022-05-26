adfac214e13d753e2c260278a6841f895cfc6f32
JoaoMorais96 <goncalomorais96@hotmail.com>
2022-05-26 15:27:11 +0100

    final form

--- AWS-SimpleVoting/tests/funding_test_wallets.py
+++ AWS-SimpleVoting/tests/funding_test_wallets.py
@@ -14,26 +14,23 @@ minAmount = 2
 #Connect to Veblocks and import the DHN contract
 #
 def init():
-    print("------------------Connect to Veblocks------------------\n")
+    print("1) Connect to Veblocks")
     #https://mainnet.veblocks.net
     #SayNode testnet node : http://3.71.71.72:8669/doc/swagger-ui/
     #SayNode mainnet node : http://3.124.193.149:8669/doc/swagger-ui/
     connector = Connect("http://3.71.71.72:8669")
-    print("------------------IMPORT DHN CONTRACT------------------\n")
+    print("2) IMPORT DHN CONTRACT")
     _contract = Contract.fromFile("./build/contracts/DHN.json")
     DHN_contract_address = '0x0867dd816763BB18e3B1838D8a69e366736e87a1'
     return connector, _contract, DHN_contract_address
 
 #Import wallets from mnemonic (this should be only one, but for know we need 2 for testing)
-def wallet_import_1(connector):
-    #MNEMONIC_1 = config('MNEMONIC_1')
-    MNEMONIC_1 = config('MNEMONIC_1')
-    testwallet1 = Wallet.fromMnemonic(MNEMONIC_1.split(', '))
-    testWallet1_address= testwallet1.getAddress()
-    print("Wallet address: " + testWallet1_address)
-    print("Wallet VET balance: " + str(connector.get_vet_balance(testWallet1_address)))
-    print("Wallet VTHO balance: " + str(connector.get_vtho_balance(testWallet1_address)))
-    return testwallet1, testWallet1_address
+def wallet_import(num):
+    mne = 'MNEMONIC_'+str(num)
+    MNEMONIC = config(mne)
+    testwallet = Wallet.fromMnemonic(MNEMONIC.split(', '))
+    testWallet_address= testwallet.getAddress()
+    return testwallet, testWallet_address
 
 def get_balance(connector,_contract, DHN_contract_address, wallet_address, num):
 
@@ -58,25 +55,29 @@ def transfer_DHN(connector, DHN_contract_address, testwallet1, receiver_address,
 
 def main():
     (connector, _contract, DHN_contract_address)=init()
-    (testwallet1, testWallet1_address)=wallet_import_1(connector)
-    wallet_two='0x306A430F0E361e96E69D650067Eba3F73307b5C4'
-    wallet_three='0x5007d0568127Bd998010Aa5a094361cE75B68B5f'
-    wallet_four='0x00712682a3aF33187570b41937A384A390DE7799'
-    wallet_five='0x72a3e156EB026b6e21327A22C8E4BB239464Eb6D'
-    wallet_six='0x9150BBa5E4875076cE98fa35EcEdD6A87f8598C6'
 
-    transfer_DHN(connector, DHN_contract_address, testwallet1, wallet_two, 2)
-    transfer_DHN(connector, DHN_contract_address, testwallet1, wallet_three, 2)
-    transfer_DHN(connector, DHN_contract_address, testwallet1, wallet_four, 2)
-    transfer_DHN(connector, DHN_contract_address, testwallet1, wallet_five, 2)
-    transfer_DHN(connector, DHN_contract_address, testwallet1, wallet_six, 2)
+    print("3) WALLETs CONNECTION")
+    (testwallet1, testWallet1_address)=wallet_import(1)
+    (testwallet2, testWallet2_address)=wallet_import(2)
+    (testwallet3, testWallet3_address)=wallet_import(3)
+    (testwallet4, testWallet4_address)=wallet_import(4)
+    (testwallet5, testWallet5_address)=wallet_import(5)
+    (testwallet6, testWallet6_address)=wallet_import(6)
 
-    time.sleep(15)
+    print("4) TRANSFERS")
+    transfer_DHN(connector, DHN_contract_address, testwallet1, testWallet2_address, 2)
+    transfer_DHN(connector, DHN_contract_address, testwallet1, testWallet3_address, 2)
+    transfer_DHN(connector, DHN_contract_address, testwallet1, testWallet4_address, 2)
+    transfer_DHN(connector, DHN_contract_address, testwallet1, testWallet5_address, 2)
+    transfer_DHN(connector, DHN_contract_address, testwallet1, testWallet6_address, 2)
+
+    time.sleep(10)
+    print("5) BALANCES")
     get_balance(connector,_contract, DHN_contract_address, testWallet1_address,1)
-    get_balance(connector,_contract, DHN_contract_address, wallet_two, 2)
-    get_balance(connector,_contract, DHN_contract_address, wallet_three, 3)
-    get_balance(connector,_contract, DHN_contract_address, wallet_four, 4)
-    get_balance(connector,_contract, DHN_contract_address, wallet_five, 5)
-    get_balance(connector,_contract, DHN_contract_address, wallet_six, 6)
+    get_balance(connector,_contract, DHN_contract_address, testWallet2_address, 2)
+    get_balance(connector,_contract, DHN_contract_address, testWallet3_address, 3)
+    get_balance(connector,_contract, DHN_contract_address, testWallet4_address, 4)
+    get_balance(connector,_contract, DHN_contract_address, testWallet5_address, 5)
+    get_balance(connector,_contract, DHN_contract_address, testWallet6_address, 6)
 
 main()
